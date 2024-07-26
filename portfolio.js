f674150cfb0be142a5d95a000800549fec24b401
armen196 <akyar99@gmail.com>
2024-07-26 14:24:15 -0700

    Got some stuff added

--- portfolio.js
+++ portfolio.js
@@ -6,7 +6,10 @@ function swap() {
     backgrounds[imageIndex].classList.add("showing");
 }
 var navBar = document.getElementById("navBar");
+var descBoxes = document.querySelectorAll('.about-me-box');
+var descBoxPos = 0;
 var stick = navBar.offsetTop;
+
 
 window.onscroll = function () {
     if (window.scrollY > stick) {
@@ -14,5 +17,25 @@ window.onscroll = function () {
     } else {
         navBar.classList.remove("sticky");
     }
+    descBoxes.forEach(box => {
+        descBoxPos = box.offsetTop;
+        if (window.scrollY > descBoxPos - 800) {
+            if (box.classList.contains("right")) {
+                box.classList.add("show-l");
+            }
+            else {
+                if (box.classList.contains("left")) {
+                    box.classList.add("show-r");
+                }
+            }
+        }
+        else {
+            box.classList.remove("show-l");
+            box.classList.remove("show-r");
+        }
+    });
+}
+function scrollToTarget(target) {
+    document.getElementById(target).scrollIntoView({ behavior: 'smooth' });
 }
 setInterval(swap, 6000);
