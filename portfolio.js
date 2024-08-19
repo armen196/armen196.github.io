c82f1bbbfa5d2265ddfda3510bb3b08017f079fa
armen196 <akyar99@gmail.com>
2024-08-18 21:55:52 -0700

    Progress

--- portfolio.js
+++ portfolio.js
@@ -2,7 +2,7 @@ let backgrounds = document.querySelectorAll(".background");
 let imageIndex = 0;
 function swap() {
     backgrounds[imageIndex].classList.remove("showing");
-    imageIndex = (imageIndex+1) % 4;
+    imageIndex = (imageIndex + 1) % 4;
     backgrounds[imageIndex].classList.add("showing");
 }
 var navBar = document.getElementById("navBar");
@@ -10,8 +10,9 @@ var descBoxes = document.querySelectorAll('.about-me-box');
 var descBoxPos = 0;
 var stick = navBar.offsetTop;
 let options = document.getElementsByClassName("option");
-
-
+var projectBoxes = document.querySelectorAll('.project-box');
+var projContainer = document.getElementsByClassName("projects-container");
+var westernImage = document.getElementById("western-image");
 window.onscroll = function () {
     if (window.scrollY > stick) {
         navBar.classList.add("sticky")
@@ -35,8 +36,32 @@ window.onscroll = function () {
             box.classList.remove("show-r");
         }
     });
+    projectBoxes.forEach(box => {
+        boxPos = box.offsetTop;
+        if (window.scrollY > boxPos - 1500) {
+            //projContainer.classList.add("show");
+            if (box.id == 'cowboy') {
+                setTimeout(() => box.classList.add("show-t"), 100)
+            }
+            else if (box.id == 'messenger') {
+                setTimeout(() => box.classList.add("show-t"), 200)
+            }
+            else {
+                box.classList.add("show-t");
+            }
+        }
+        else {
+            box.classList.remove("show-t");
+        }
+    });
+    if (window.scrollY > westernImage.offsetTop - 790) {
+        console.log("should pop up");
+        westernImage.classList.add("show-western-image");
+    } else {
+        westernImage.classList.remove("show-western-image");
+    }
 }
-function scrollToTarget(target, adj=0) {
+function scrollToTarget(target, adj = 0) {
     //document.getElementById(target).scrollIntoView({ behavior: 'smooth' });
     const yOffset = 200;
     const el = document.getElementById(target);
